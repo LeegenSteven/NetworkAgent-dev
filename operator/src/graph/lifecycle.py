66551fb5fc0e5d7ce7f112fb0652b652eabf95e6
Laurent Julliard <laurent@moldus.org>
2024-09-13 15:17:06 +0200

    graph db construction
    graphdb branch merge

--- operator/src/graph/lifecycle.py
+++ operator/src/graph/lifecycle.py
@@ -5,16 +5,17 @@ from graph.lifecycle_tasks import *
 
 logger = logging.getLogger(__name__)
 
-@kopf.on.create('google.dev', 'v1', kopf.EVERYTHING, labels = {'graph': 'true'})
-async def create_node(body, spec, metadata, name, logger, **kwargs):
+# Catch create events
+# TODO: see if you can be more specific in the Kopf resource
+# specification without loosing genericity too much
+@kopf.on.create(kopf.EVERYTHING, labels = {'graph': 'true'})
+async def create_node(body, spec, meta, uid, name, logger, **kwargs):
   logger.info("Create graph network node")
   success = False
 
   # We know that this node must be added to the graph
   # thanks to the filter on graph label
-  name = spec.get('name')
-  kind = spec.get('kind')
-  uid = metadata.get('uid')
+  kind = body.get('kind')
   if uid is None:
     raise kopf.PermanentError("Graph node without UID")
   else:
@@ -24,30 +25,27 @@ async def create_node(body, spec, metadata, name, logger, **kwargs):
 
   # if parent name doesn't exist it is the root node
   # that represents the network service deployed
-  owner_ref = metadata.get('owner_references')
-  if owner_ref is None:
-    parent_uid = owner_ref.get('UID')
+  owner_ref = meta.get('ownerReferences')
+  if owner_ref is not None:
+    parent_uid = owner_ref[0]['uid']
     if parent_uid is None:
-      raise kopf.PermanentError("Graph node without parent UID")
+      raise kopf.PermanentError("Graph child node without parent UID")
     
-    logger.info("Linking node %s to parent node %s\n", uid[-9:-1], parent_uid[-9:-1])
-    success |= create_network_connection(uid, parent_uid)
+    logger.info("Linking node %s to parent node %s", uid, parent_uid)
+    success |= create_network_connection(parent_uid, uid)
 
-    logger.info("Create node success = %s", success)
+    logger.info("Create node '%s' (success: %s)", name, success)
     if not success:
-      logger.info(json.dumps(body, indent=2))
-      raise kopf.TemporaryError("Create node error", delay=15)
+      raise kopf.TemporaryError("Create node error", retry=1, delay=15)
 
-
-@kopf.on.delete('google.dev', 'v1', kopf.EVERYTHING, labels = {'graph': 'true'})
-async def deletetest(body, spec, metadata, logger, **kwargs):
+# Catch delete events
+@kopf.on.delete(kopf.EVERYTHING, labels = {'graph': 'true'})
+async def delete_node(body, spec, uid, name, logger, **kwargs):
   logger.info("Delete graph network node")
   success = False
 
-  # Get the node unique id
-  name = spec.get('name')
-  kind = spec.get('kind')
-  uid = metadata.get('uid')
+  # Check the node unique id
+  kind = body.get('kind')
   if uid is None:
     raise kopf.PermanentError("Graph node without UID")
   else:
@@ -55,14 +53,13 @@ async def deletetest(body, spec, metadata, logger, **kwargs):
 
   # delete network connections first because of database
   # consistency foreign key rule
-  logger.info("Deleting network connections for uid %s \n", uid[-9:-1])
+  logger.info("Deleting network connections for uid %s (%s:%s)", uid, kind, name)
   success |= delete_network_connection(uid)
   if success:
-    logger.info("Deleting network nodes for uid %s \n", uid[-9:-1])
+    logger.info("Deleting network nodes for uid %s \n", uid)
     success |= delete_network_node(uid)
 
-  logger.info("Create node success = %s", success)
+  logger.info("Delete node '%s' (success: %s)", name, success)
   if not success:
-    logger.info(json.dumps(body, indent=2))
-    raise kopf.TemporaryError("Delete node error", delay=15)
+    raise kopf.TemporaryError("Delete node error", retry=1, delay=15)
 
--- operator/src/graph/lifecycle_tasks.py
+++ operator/src/graph/lifecycle_tasks.py
@@ -5,15 +5,15 @@ import json
 from google.cloud import spanner
 
 SQL_TEMPLATES = {
-  'insert_nw_node': "INSERT NetworkNode (id, kind, name, display_name, node_properties)" 
-                    " VALUES ('{id}', '{kind}', '{name}', '{display_name}', '{crd}')",
-  'delete_nw_node': "DELETE FROM NetworkNode WHERE id = '{id}'",
-  'insert_nw_cnx': "INSERT NetworkConnection (id, to_id) VALUES ('{id}', '{to_id}')",
+  'create_nw_node': "INSERT NetworkNode (id, kind, name, display_name, node_property)" 
+                    " VALUES ('{id}', '{kind}', '{name}', '{display_name}', {crd})",
+  'delete_nw_node': "DELETE FROM NetworkNode WHERE to_id = '{id}'",
+  'create_nw_cnx': "INSERT NetworkConnection (id, to_id) VALUES ('{id}', '{to_id}')",
   'delete_nw_cnx': "DELETE FROM NetworkConnection WHERE id = '{id}'"
 }
 
 # Connect to Spanner database
-async def spanner_connect():
+def spanner_connect():
   spanner_client = spanner.Client()
   instance = spanner_client.instance('networktopology-instance')
   database = instance.database('networktopology-db')
@@ -22,30 +22,106 @@ async def spanner_connect():
 database = spanner_connect()
 logger = logging.getLogger(__name__)
 
-async def create_network_node(crd, name, kind, uid):
-  tmpl = SQL_TEMPLATES['insert_nw_node']
-  sql = tmpl.format(id=uid, kind=kind, name=name, display_name="{kind} ({name})", crd=json.dumps(crd))
-  row_ct = database.run_in_transaction(sql)
-  logger.info ("{} node(s) inserted.".format(row_ct))
-  return (row_ct == 1)
+# ------------------------------------------
+# Create a network node
+# ------------------------------------------
+def create_network_node(crd, name, kind, uid):
 
-async def delete_network_node(uid):
-  tmpl = SQL_TEMPLATES['delete_nw_node']
-  sql = tmpl.format(id=uid)
-  row_ct = database.run_in_transaction(sql)
-  logger.info ("{} node(s) deleted.".format(row_ct))
-  return (row_ct == 1)
+  def sql_create_network_node(transaction):
+    tmpl = SQL_TEMPLATES['create_nw_node']
+    display_name="{} ({})".format(kind, name)
+    # TODO: Do not insert CRD for now. The serialized JSON uses single quotes
+    # which causes the value in INSERT to fail
+    sql = tmpl.format(id=uid, kind=kind, name=name, display_name=display_name, crd='NULL')
+    logger.info("SQL: {}".format(sql))
+    return transaction.execute_update(sql)
+  
+  row_ct = 0
+  success = True
+  try:
+    row_ct = database.run_in_transaction(sql_create_network_node)
+  except Exception as e:
+    success = False
+    logger.error("SQL error: {}".format(e))
 
-async def create_network_connection(uid, parent_uid):
-  tmpl = SQL_TEMPLATES['insert_nw_cnx']
-  sql = tmpl.format(id=uid, to_id=parent_uid)
-  row_ct = database.run_in_transaction(sql)
-  logger.info ("{} connection(s) inserted.".format(row_ct))
-  return (row_ct == 1)
+  if success:
+    logger.info("{} node inserted (row count: {})".format(uid,row_ct))
+  else:
+    logger.error("Node {} creation failed".format(uid))
+  return success
 
-async def delete_network_connection(uid):
-  tmpl = SQL_TEMPLATES['delete_nw_cnx']
-  sql = tmpl.format(id=uid)
-  row_ct = database.run_in_transaction(sql)
-  logger.info ("{} connection(s) deleted.".format(row_ct))
-  return (row_ct > 0)
+# ------------------------------------------
+# Delete a network node
+# ------------------------------------------
+
+def delete_network_node(uid):
+
+  def sql_delete_network_node(transaction):
+    tmpl = SQL_TEMPLATES['delete_nw_node']
+    sql = tmpl.format(id=uid)
+    logger.info("SQL: {}".format(sql))
+    return transaction.execute_update(sql)
+  
+  row_ct = 0
+  success = True
+  try:
+    row_ct = database.run_in_transaction(sql_delete_network_node)
+  except Exception as e:
+    success = False
+    logger.error("SQL error: {}".format(e))
+
+  if success:
+    logger.info("{} node deleted (row count: {})".format(uid,row_ct))
+  else:
+    logger.error("Node {} deletion failed".format(uid))
+  return success
+
+# ------------------------------------------
+# Create a network connection
+# ------------------------------------------
+def create_network_connection(parent_uid, uid):
+
+  def sql_create_network_connection(transaction):
+    tmpl = SQL_TEMPLATES['create_nw_cnx']
+    sql = tmpl.format(id=parent_uid, to_id=uid)
+    logger.info("SQL: {}".format(sql))
+    return transaction.execute_update(sql)
+  
+  row_ct = 0
+  success = True
+  try:
+    row_ct = database.run_in_transaction(sql_create_network_connection)
+  except Exception as e:
+    success = False
+    logger.error("SQL error: {}".format(e))
+
+  if success:
+    logger.info("{} -> {} connection inserted (row count: {})".format(parent_uid,uid,row_ct))
+  else:
+    logger.error("{} -> {} connection creation failed".format(parent_uid, uid))
+  return success
+
+# ------------------------------------------
+# Delete network connections
+# ------------------------------------------
+def delete_network_connection(uid):
+
+  def sql_delete_network_connection(transaction):
+    tmpl = SQL_TEMPLATES['delete_nw_cnx']
+    sql = tmpl.format(id=uid)
+    logger.info("SQL: {}".format(sql))
+    return transaction.execute_update(sql)
+  
+  row_ct = 0
+  success = True
+  try:
+    row_ct = database.run_in_transaction(sql_delete_network_connection)
+  except Exception as e:
+    success = False
+    logger.error("SQL error: {}".format(e))
+
+  if success:
+    logger.info("{} connection(s) deleted for node {}".format(row_ct, uid))
+  else:
+    logger.error("Connection {} deletion failed".format(uid))
+  return success

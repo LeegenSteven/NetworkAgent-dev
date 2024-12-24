c9c9e5007bc591042b9c34681e2c589df5d2e034
Laurent Julliard <laurent@moldus.org>
2024-12-24 14:30:01 +0100

    Leaner agent. Fully langchain way

--- networkagent/src/agent/networkagent.py
+++ networkagent/src/agent/networkagent.py
@@ -3,20 +3,18 @@ from langgraph.checkpoint.memory import MemorySaver
 from langchain_google_vertexai import HarmBlockThreshold, HarmCategory
 from langchain.callbacks.manager import CallbackManager
 from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, END, START
 from langgraph.graph.message import add_messages
 from typing import TypedDict, Annotated, Literal, cast
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, ToolMessage, AIMessage
 from langchain_core.runnables.config import RunnableConfig
-from langgraph.prebuilt import ToolNode
-from langchain_core.tools import BaseTool, InjectedToolArg
-from langchain_core.tools import tool as create_tool
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langgraph.prebuilt import ToolNode, tools_condition
 from agent.tools import *
 import google.auth
 import logging
-import json
-from utils.tool_helpers import create_tool_node_with_fallback
 import os
 
 # Uncomment lines below to debug the Langchain flow of operations
@@ -31,7 +29,7 @@ class NetworkAgentState(TypedDict):
 
 class NetworkAgent:
     def __init__(self):
-        logger.info("loading networkagent credentials from path = %s", os.getcwd())
+        logger.debug("loading networkagent credentials from path = %s", os.getcwd())
 
         # agent memory
         memory = MemorySaver()
@@ -40,15 +38,7 @@ class NetworkAgent:
 
         safe_tools=[getCustomerLocations, getCustomerApplications, getServiceDefinitions, getServices]#, getServicePerformanceMetrics]
         unsafe_tools=[createService, deleteService] #, createTest, deleteTest]
-        tools = safe_tools+unsafe_tools
-
-        # build tools map for custom tool node
-        self.tools_map ={}
-        for tool_ in tools:
-            logger.debug(tool_)
-            if not isinstance(tool_, BaseTool):
-                tool_ = cast(BaseTool, create_tool(tool_))
-            self.tools_map[tool_.name]=tool_
+        agent_tools = safe_tools+unsafe_tools
 
         safety_settings = {
             HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_ONLY_HIGH,
@@ -57,7 +47,7 @@ class NetworkAgent:
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
             HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
         }
-        llm = ChatVertexAI(model_name="gemini-1.5-flash-002", 
+        self.model = ChatVertexAI(model_name="gemini-1.5-flash-002", 
                            temperature=0,
                            credentials=credentials,
                            request_parallelism=1,
@@ -68,6 +58,7 @@ class NetworkAgent:
                            project=os.getenv("GOOGLE_PROJECT"),
                            location=os.getenv("GOOGLE_REGION"),
                            callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+        self.model_with_tools = self.model.bind_tools(agent_tools)
 
         system_prompt = """
             You are a networking engineer specialist helper bot.
@@ -83,7 +74,7 @@ class NetworkAgent:
 
             Greet the users and ask how you can help them today.
             - If necessary, seek clarifying details on what their request is.
-            - If the request involoves any of the following, use the tools provided to help the user with their task:
+            - If the request involves any of the following, use the tools provided to help the user with their task:
                 - get a list of available networking services providing a summary description of each and the data needed to instantiate them
                 - get a set of network locations based on a customer
                 - get the currently deployed networking services based on a customer
@@ -93,8 +84,8 @@ class NetworkAgent:
                 - Delete an existing connectivity test for a customer IT application
                 - Get the performance metrics for a deployed connectivity service
                 - The networking orchestrator itself uses Kubernetes CRDs to control the networking operations state. Hence the responses from this Tool will be in a kubernetes CRD format.
-            - If the request involves anything else, try your best to answer, but explain that you are an agent to be used specfically for managing network connectivity services and you should then list the capabilities above
-
+            - if the request involves anything else, try your best to answer, but explain that you are an agent to be used specifically 
+              for managing network connectivity services and you should then list the capabilities above
             """
 
         network_agent_prompt=ChatPromptTemplate.from_messages(
@@ -104,111 +95,50 @@ class NetworkAgent:
             ]
         )
 
-        self.network_agent_runnable = network_agent_prompt | llm.bind_tools(
-            tools
+        self.network_agent_runnable = network_agent_prompt | self.model_with_tools
+        
+        networkAgentGraph = StateGraph(NetworkAgentState)
+        networkAgentGraph.add_node("agent_model", self.agent_model)
+        networkAgentGraph.add_node("agent_tools", ToolNode(agent_tools))
+        networkAgentGraph.add_conditional_edges(
+            "agent_model",
+            # Assess agent decision
+            tools_condition,
+            {
+                # Translate the condition outputs to nodes in our graph
+                "tools": "agent_tools",
+                END: END,
+            },
+            #self.should_continue
         )
+        networkAgentGraph.add_edge(START, "agent_model")
 
-        networkGraph = StateGraph(NetworkAgentState)
-        networkGraph.add_node("agent", self.call_model)
-        networkGraph.add_node("tools", self.call_tool)
-        networkGraph.set_entry_point("agent")
-        networkGraph.add_conditional_edges(
-            "agent",
-            self.should_continue
-        )
-
-        self.networkAgentApp = networkGraph.compile(
+        self.networkAgentApp = networkAgentGraph.compile(
             checkpointer=memory,
             # interrupt_before=[
             #     "unsafe_tools"
             # ]
         )
-
-    def should_continue(self, state: NetworkAgentState) -> Literal["__end__", "tools"]:
-        logger.debug(state)
-        messages = state["messages"]
-
-        last_message = messages[-1]
-#       if hasattr(last_message, 'tool_calls') and (not last_message.tool_calls):
-        if not last_message.tool_calls:
-            return "__end__"
-        else:
-            return "tools"
     
-    def call_model(self, state: NetworkAgentState, config: RunnableConfig):
-        logger.debug("calling model")
-        try:
-            logger.debug("<MESSAGES>")
-            for m in state['messages']:
-                if isinstance(m, AIMessage):
-                    logger.debug("AIMESSAGE")
-                elif isinstance(m, HumanMessage):
-                    logger.debug("HUMAN")
-                elif isinstance(m, ToolMessage):
-                    logger.debug("TOOL")
-                else:
-                    logger.debug("UNKNOWN")
-                    logger.debug(m)
-            result = None
-            while True:
-                # Append to state
-                state = {**state}
-                # Invoke the tool-calling LLM
-                result = self.network_agent_runnable.invoke(state, config)
-                # If it is a tool call -> response is valid
-                # If it has meaningful text -> response is valid
-                # Otherwise, we re-prompt it b/c response is not meaningful
-                if result is None or not result.tool_calls and (
-                    not result.content
-                    or isinstance(result.content, list)
-                    and not result.content[0].get("text")
-                ):
-                    messages = state["messages"] + [("user", "Respond with a real output.")]
-                    state = {**state, "messages": messages}
-                    logger.debug("GOT DODGY ANSWER FROM GEMINI")
-                else:
-                    break
-            logger.debug(result.to_json())
-            return {"messages": result}
-        
-        except Exception as e:
-            logger.debug("Caught Exception")
-            logger.debug(e)
-            logger.debug("*** RESULT IN EXCEPTION ***")
-            logger.debug(result)
+    def agent_model(self, state: NetworkAgentState, config: RunnableConfig):
+        """
+        Invokes the agent model to generate a response based on the current state. Given
+        the question, it will decide to answer using the tools, or simply end.
 
-    def call_tool(self, state: NetworkAgentState):
-        messages = state["messages"]
-        last_message = messages[-1]
-        logger.debug("+++++++++++++++++++++++++    TOOL CALL  +++++++++++++++++++++++++++++++++")
-        logger.debug(last_message)
-        output_messages = []
-#        if not hasattr(last_message, 'tool_calls'): return {"messages": "No tool to be called"}
-        for tool_call in last_message.tool_calls:
-            try:
-                logger.debug(tool_call)
-                tool_result = self.tools_map[tool_call["name"]].invoke(tool_call["args"])
-                output_messages.append(
-                    ToolMessage(
-                        content=tool_result,
-                        name=tool_call["name"],
-                        tool_call_id=tool_call["id"],
-                    )
-                )
-            except Exception as e:
-                # Return the error if the tool call fails
-                logger.debug('Error')
-                # output_messages.append(
-                #     ToolMessage(
-                #         content="",
-                #         name=tool_call["name"],
-                #         tool_call_id=tool_call["id"],
-                #         additional_kwargs={"error": e},
-                #     )
-                # )
-        logger.debug(output_messages)
-        logger.debug("+++++++++++++++++++++++++    TOOL OUTPUT  +++++++++++++++++++++++++++++++++")
-        return {"messages": output_messages}
+        Args:
+            state (messages): The current state
+
+        Returns:
+            dict: The updated state with the agent response appended to messages
+        """
+        logger.debug("---CALL AGENT---")
+        logger.debug(f"state: {state}")
+
+        response = self.network_agent_runnable.invoke(state)
+        # We return a list, because this will get added to the existing list
+        state["messages"] = response
+        return state
+
 
 
     def run(self, question):

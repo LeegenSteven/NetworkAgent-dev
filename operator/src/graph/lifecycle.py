e5a107d6b9c3cd4d9647bb35e502e754455d7174
Brian Naughton <briannaughton@google.com>
2026-02-08 21:40:16 +0000

    disable ufw

--- operator/src/graph/lifecycle.py
+++ operator/src/graph/lifecycle.py
@@ -22,6 +22,16 @@ logger = logging.getLogger(__name__)
 
 # --- VyOS Handlers ---
 
+# GRAPH networkGraph
+# MATCH router_path = (r1:PhysicalRouter)-[:HasInterface]->(:PhysicalInterface)
+#       -[:ConnectsTo]->(:PhysicalLink)
+#       -[:LinkedTo]->(:PhysicalInterface)
+#       <-[:HasInterface]-(r2:PhysicalRouter)
+# WHERE r1.id < r2.id
+# RETURN SAFE_TO_JSON(router_path) AS router_path
+# LIMIT 40;
+
+
 @kopf.on.event('vyosinfrastructures.google.dev', id='vyosinfrastructure-graph')
 async def obj_vyosinfrastructure(body, spec, name, uid, logger, event, **kwargs):
     if event['type'] == 'DELETED':
